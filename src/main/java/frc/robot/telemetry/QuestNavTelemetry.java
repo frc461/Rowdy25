3f6d83e0f18430f457d01f712401b7d41b4b827c
Gerseneck <wang14240@gmail.com>
2024-12-20 12:13:33 -0500

    feat: all the necessary questnav telemetry (i think)

--- src/main/java/frc/robot/telemetry/QuestNavTelemetry.java
+++ src/main/java/frc/robot/telemetry/QuestNavTelemetry.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.SignalLogger;
 
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.FloatArraySubscriber;
-import edu.wpi.first.networktables.IntegerPublisher;
 import edu.wpi.first.networktables.IntegerSubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,17 +13,13 @@ import frc.robot.Constants;
 public class QuestNavTelemetry {
     NetworkTable oculusQuestTable = Constants.NT_INSTANCE.getTable("oculus");
 
-    // TODO: figure out what topic miso and mosi does
-    private IntegerSubscriber questMisoTopic = Constants.NT_INSTANCE.getIntegerTopic("miso").subscribe(0);
-    private IntegerPublisher questMosiTopic = Constants.NT_INSTANCE.getIntegerTopic("mosi").publish();
-
     private IntegerSubscriber questFrameCountTopic = oculusQuestTable.getIntegerTopic("frameCount").subscribe(0);
     private DoubleSubscriber questTimestampTopic = oculusQuestTable.getDoubleTopic("timestamp").subscribe(0.0f);
     private DoubleSubscriber questBatteryTopic = oculusQuestTable.getDoubleTopic("batteryLevel").subscribe(0.0f);
     private FloatArraySubscriber questPositionTopic = oculusQuestTable.getFloatArrayTopic("position").subscribe(new float[] {0.0f, 0.0f, 0.0f});
     private FloatArraySubscriber questQuaternionTopic = oculusQuestTable.getFloatArrayTopic("quaternion").subscribe(new float[] {0.0f, 0.0f, 0.0f, 0.0f});
     private FloatArraySubscriber questEulerAnglesTopic = oculusQuestTable.getFloatArrayTopic("eulerAngles").subscribe(new float[] {0.0f, 0.0f, 0.0f});
-    
+
     private long questFrameCount;
     private double questTimestamp;
     private double questBattery;
@@ -53,9 +48,19 @@ public class QuestNavTelemetry {
         SmartDashboard.putNumber("quest-frame-count", questFrameCount);
         SmartDashboard.putNumber("quest-timestamp", questTimestamp);
         SmartDashboard.putNumber("quest-battery-level", questBattery);
+
         SmartDashboard.putNumber("quest-x-pos", questPosition[0]);
         SmartDashboard.putNumber("quest-y-pos", questPosition[1]);
-        SmartDashboard.putNumber("quest-z-pos", questPosition[2]); // probably z pos, might be angle
-    }
+        SmartDashboard.putNumber("quest-z-pos", questPosition[2]);
 
+        SmartDashboard.putNumber("quest-quaternion-x", questQuaternion[0]);
+        SmartDashboard.putNumber("quest-quaternion-y", questQuaternion[1]);
+        SmartDashboard.putNumber("quest-quaternion-z", questQuaternion[2]);
+        SmartDashboard.putNumber("quest-quaternion-w", questQuaternion[3]);
+
+
+        SmartDashboard.putNumber("quest-euler-angle-x", questEulerAngles[0]);
+        SmartDashboard.putNumber("quest-euler-angle-y", questEulerAngles[1]);
+        SmartDashboard.putNumber("quest-euler-angle-z", questEulerAngles[2]);
+    }
 }
