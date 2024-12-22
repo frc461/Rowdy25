f2a70a30de91fa5a78e14dd2c6a266b8040be117
ez500 <eugenezhang500@gmail.com>
2024-12-21 21:56:27 -0500

    update pose2d telemetry photon

--- src/main/java/frc/robot/telemetry/VisionTelemetry.java
+++ src/main/java/frc/robot/telemetry/VisionTelemetry.java
@@ -8,42 +8,48 @@ import frc.robot.util.VisionUtil;
 
 public class VisionTelemetry {
     private final Swerve swerve;
-    private final NetworkTable limelightTelemetryTable = Constants.NT_INSTANCE.getTable("LimelightTelemetry");
     private final NetworkTable visionTelemetryTable = Constants.NT_INSTANCE.getTable("VisionTelemetry");
+    private final NetworkTable limelightTelemetryTable = Constants.NT_INSTANCE.getTable("LimelightTelemetry");
+    private final NetworkTable photonTelemetryTable = Constants.NT_INSTANCE.getTable("PhotonTelemetry");
     private final NetworkTable questNavTelemetryTable = Constants.NT_INSTANCE.getTable("oculus");
+
+    private final StringPublisher poseEstimatePub = visionTelemetryTable.getStringTopic("Robot Estimated Pose").publish();
 
     private final StringPublisher megaTagOnePub = limelightTelemetryTable.getStringTopic("MegaTag Pose").publish();
     private final DoublePublisher nearestTagDistPub = limelightTelemetryTable.getDoubleTopic("Nearest Tag Distance").publish();
     private final BooleanPublisher canAddMeasurementsPub = limelightTelemetryTable.getBooleanTopic("Adding Limelight Measurements").publish();
 
-    private final StringPublisher poseEstimatePub = visionTelemetryTable.getStringTopic("Robot Estimated Pose").publish();
+    private final StringPublisher photonPosePub = photonTelemetryTable.getStringTopic("Photon Pose").publish();
 
-    private final StringPublisher questPrettyPositionTopic = questNavTelemetryTable.getStringTopic("Quest Position").publish();
-    private final StringPublisher questPrettyRotationTopic = questNavTelemetryTable.getStringTopic("Quest Rotation").publish();
+    private final StringPublisher questPositionTopic = questNavTelemetryTable.getStringTopic("Quest Position").publish();
+    private final StringPublisher questRotationTopic = questNavTelemetryTable.getStringTopic("Quest Rotation").publish();
 
     public VisionTelemetry(Swerve swerve) {
         this.swerve = swerve;
     }
 
     public void publishValues() {
-        Pose2d megaTagPose = VisionUtil.Limelight.getMegaTagOnePose();
-        double megaTagX = megaTagPose.getTranslation().getX();
-        double megaTagY = megaTagPose.getTranslation().getY();
-        double megaTagYaw = megaTagPose.getRotation().getDegrees();
-        megaTagOnePub.set("X: " + megaTagX + ", Y: " + megaTagY + ", Yaw: " + megaTagYaw);
-
         Pose2d estimatedPose = swerve.getEstimatedPose();
-        double estimateX = estimatedPose.getTranslation().getX();
-        double estimateY = estimatedPose.getTranslation().getY();
+        double estimateX = estimatedPose.getX();
+        double estimateY = estimatedPose.getY();
         double estimateYaw = estimatedPose.getRotation().getDegrees();
         poseEstimatePub.set("X: " + estimateX + ", Y: " + estimateY + ", Yaw: " + estimateYaw);
 
+        Pose2d megaTagPose = VisionUtil.Limelight.getMegaTagOnePose();
+        double megaTagX = megaTagPose.getX();
+        double megaTagY = megaTagPose.getY();
+        double megaTagYaw = megaTagPose.getRotation().getDegrees();
+        megaTagOnePub.set("X: " + megaTagX + ", Y: " + megaTagY + ", Yaw: " + megaTagYaw);
         nearestTagDistPub.set(VisionUtil.Limelight.getNearestTagDist());
-
         canAddMeasurementsPub.set(VisionUtil.Limelight.isTagClear());
 
-        questPrettyPositionTopic.set("X: " + VisionUtil.Oculus.getX() + ", Y: " + VisionUtil.Oculus.getY() + ", Z: " + VisionUtil.Oculus.getZ());
+        Pose2d photonPose = VisionUtil.Photon.BW.getPhotonPose();
+        double photonPoseX = photonPose.getX();
+        double photonPoseY = photonPose.getY();
+        double photonPoseYaw = photonPose.getRotation().getDegrees();
+        photonPosePub.set("X: " + photonPoseX + ", Y: " + photonPoseY + ", Yaw: " + photonPoseYaw);
 
-        questPrettyRotationTopic.set("Pitch: " + VisionUtil.Oculus.getPitch() + ", Yaw: " + VisionUtil.Oculus.getYaw() + ", Roll: " + VisionUtil.Oculus.getRoll());
+        questPositionTopic.set("X: " + VisionUtil.QuestNav.getX() + ", Y: " + VisionUtil.QuestNav.getY() + ", Z: " + VisionUtil.QuestNav.getZ());
+        questRotationTopic.set("Pitch: " + VisionUtil.QuestNav.getPitch() + ", Yaw: " + VisionUtil.QuestNav.getYaw() + ", Roll: " + VisionUtil.QuestNav.getRoll());
     }
 }
