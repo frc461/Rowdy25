02c0d4c99e55a267421d815faf4ce6d86b544913
Gerseneck <wang14240@gmail.com>
2024-12-24 23:50:25 -0500

    refactor: refactor

--- src/main/java/frc/robot/telemetry/LocalizationTelemetry.java
+++ src/main/java/frc/robot/telemetry/LocalizationTelemetry.java
@@ -88,7 +88,7 @@ public class LocalizationTelemetry {
         questNavTelemetryTable.addListener("questDisconnect", EnumSet.of(NetworkTableEvent.Kind.kDisconnected), (table, key, event) -> {
             if (!event.is(NetworkTableEvent.Kind.kDisconnected)) { return; }
 
-            DogLog.logFault(Constants.Logger.RobotFault.QUEST_DISCONNECT);
+            DogLog.logFault(Constants.Logger.QuestFault.QUEST_DISCONNECT);
             Elastic.sendNotification(new Elastic.Notification(Elastic.Notification.NotificationLevel.ERROR, "Quest Nav", "Quest has been disconnected! Press B to switch to PoseEstimator."));
         });
 
@@ -97,12 +97,12 @@ public class LocalizationTelemetry {
 
             if (Arrays.stream(questBatterySub.readQueueValues()).noneMatch(x -> x <= 0.005)
                     && questBatterySub.get() <= 0.005) {
-                DogLog.logFault(Constants.Logger.RobotFault.QUEST_DIED);
+                DogLog.logFault(Constants.Logger.QuestFault.QUEST_DIED);
                 Elastic.sendNotification(new Elastic.Notification(Elastic.Notification.NotificationLevel.ERROR, "Quest Nav", "Quest ran out of battery! Press B to switch to PoseEstimator."));
             }
             if (Arrays.stream(questBatterySub.readQueueValues()).noneMatch(x -> x <= 0.1)
                     && questBatterySub.get() <= 0.1) {
-                DogLog.logFault(Constants.Logger.RobotFault.QUEST_LOW_BATTERY);
+                DogLog.logFault(Constants.Logger.QuestFault.QUEST_LOW_BATTERY);
                 Elastic.sendNotification(new Elastic.Notification(Elastic.Notification.NotificationLevel.WARNING, "Quest Nav", "Quest has less than 10% battery left! Current Percent: " + (int) (questBatterySub.get() * 100)));
             }
         });
