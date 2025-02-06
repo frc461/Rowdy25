f681e2542fd78db79e006f459b3766ba3f0cb8a1
ez500 <eugenezhang500@gmail.com>
2025-02-06 00:18:06 -0500

    refactor v2

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.VoltageConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import com.reduxrobotics.canand.CanandEventLoop;
 import com.reduxrobotics.sensors.canandcolor.Canandcolor;
@@ -14,14 +13,11 @@ import com.reduxrobotics.sensors.canandcolor.CanandcolorSettings;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.util.Lights;
 
 import frc.robot.constants.Constants;
-
-import java.util.Set;
 
 public class Intake extends SubsystemBase {
     public enum States {
