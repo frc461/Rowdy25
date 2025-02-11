abf1502d0c62eaac68f35aafd9b94b8d51f17468
ez500 <eugenezhang500@gmail.com>
2025-02-11 17:23:26 -0500

    basic coral scoring keybinds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ import dev.doglog.DogLogOptions;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.autos.AutoChooser;
 import frc.robot.commands.*;
@@ -20,7 +19,6 @@ import frc.robot.subsystems.pivot.Pivot;
 import frc.robot.subsystems.wrist.Wrist;
 import frc.robot.util.SysID;
 import frc.robot.util.Lights;
-import frc.robot.util.VisionUtil;
 
 public class RobotContainer {
     /* Subsystems */
@@ -94,10 +92,12 @@ public class RobotContainer {
      * Joysticks:
      * Left: Rotate pivot
      * Right: Rotate wrist
+     * Left Button: Click - manually temp toggle between lower and higher algae reef intake level
+     * Right Button: Click - manually temp toggle between processor and net height
      *
      * Bumpers:
-     * Left: Click - manually temp toggle between lower and higher algae reef intake level
-     * Right: Click - manually temp toggle between processor and net height
+     * Left: Click - wait until coral is in view then align with then intake coral (ground)
+     * Right: Click - wait until algae is in view then align with then intake algae (ground)
      *
      * Buttons:
      * A: Click - Climb stage, Click Again - stow slowly
@@ -128,7 +128,7 @@ public class RobotContainer {
         drive forward with left joystick negative Y (forward),
         drive left with left joystick negative X (left),
         rotate counterclockwise with right joystick negative X (left) */
-        swerve.setDefaultCommand( // TODO: CONTROL MAX SPEED
+        swerve.setDefaultCommand(
                 swerve.driveFieldCentric(
                         driverXbox::getLeftY,
                         driverXbox::getLeftX,
@@ -156,13 +156,16 @@ public class RobotContainer {
         );
 
         wrist.setDefaultCommand(
-                new WristCommand(wrist, () -> -opXbox.getRightY(), pivot::getPosition)
+                new WristCommand(wrist, () -> -opXbox.getRightY(), pivot::getPosition, elevator::getPosition)
         );
     }
 
     private void configureBindings() {
+        // IMPORTANT: WHEN BINDING DRIVER BUTTONS, TRIGGERS NEED TO BE ON FALSE ESPECIALLY WITH BINDINGS THAT INITIATE DRIVE AUTOMATION UPON HOLD DEBOUNCE
+        // SO FIGURE OUT LOGIC CORRECTLY AND CAREFULLY
+
         // (set to ground state and if holding then wait until there's an object then drive to it)
-        driverXbox.leftBumper().and(driverXbox.leftTrigger().negate()).onTrue(
+        driverXbox.leftBumper().negate().and(driverXbox.leftTrigger().negate()).onTrue(
                 new InstantCommand(robotStates::toggleGroundCoralState)
         );
         driverXbox.leftBumper().and(driverXbox.leftTrigger()).onTrue(new InstantCommand(robotStates::setStowState));
@@ -171,6 +174,20 @@ public class RobotContainer {
                 ? new InstantCommand(robotStates::toggleHighReefAlgaeState) 
                 : new InstantCommand(robotStates::toggleLowReefAlgaeState)
         );
+
+        opXbox.povRight().onTrue(new InstantCommand(robotStates::toggleL1CoralState));
+        opXbox.povUp().onTrue(new InstantCommand(robotStates::toggleL2CoralState));
+        opXbox.povLeft().onTrue(new InstantCommand(robotStates::toggleL3CoralState));
+        opXbox.povDown().onTrue(new InstantCommand(robotStates::toggleL4CoralState));
+
+        opXbox.leftBumper().onTrue(new InstantCommand(robotStates::toggleGroundCoralState));
+        opXbox.rightBumper().onTrue(new InstantCommand(robotStates::toggleGroundAlgaeState));
+
+        opXbox.b().onTrue(elevator.nearestAlgaeIsHigh(swerve.localizer.getStrategyPose())
+                ? new InstantCommand(robotStates::toggleHighReefAlgaeState)
+                : new InstantCommand(robotStates::toggleLowReefAlgaeState)
+        );
+        opXbox.x().onTrue(new InstantCommand(robotStates::toggleCoralStationState));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
