e982489de253e0e03fff3d8f55ed6a062982dadd
ez500 <eugenezhang500@gmail.com>
2025-02-21 20:52:00 -0500

    wait for some time

--- src/main/java/frc/robot/autos/AutoManager.java
+++ src/main/java/frc/robot/autos/AutoManager.java
@@ -12,10 +12,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.RobotStates;
 import frc.robot.autos.routines.AutoEventLooper;
 import frc.robot.autos.routines.AutoTrigger;
@@ -98,6 +95,7 @@ public final class AutoManager {
         triggers.add(autoEventLooper.addTrigger(
                 "outtake",
                 () -> new WaitUntilCommand(robotStates.atState) // TODO WAIT (PATHFINDING WORKS): JUST WAIT UNTIL STOW STATE IF AUTO SCORING WORKS (ALSO L140)
+                        .andThen(new WaitCommand(0.35))
                         .andThen(robotStates::toggleAutoLevelCoralState)
                         .andThen(new WaitUntilCommand(robotStates.stowState))
         ));
