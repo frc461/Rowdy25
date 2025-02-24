3a8f64f067c410f8e590ab914170095c2e8215c0
ez500 <eugenezhang500@gmail.com>
2025-02-24 00:17:39 -0500

    better coral station points

--- src/main/java/frc/robot/autos/Pathfinder.java
+++ src/main/java/frc/robot/autos/Pathfinder.java
@@ -161,11 +161,19 @@ public final class Pathfinder {
         for (Pose2d pose : FieldUtil.Reef.getRobotPosesAtEachBranch()) {
             System.out.println("X: " + pose.getX() + ", Y: " + pose.getY() + ", Angle: " + pose.getRotation().getDegrees());
         }
-        Pose2d station1Pose = new Pose2d(Units.inchesToMeters(67.02), Units.inchesToMeters(317), FieldUtil.AprilTag.ID_13.pose2d.getRotation())
+        Pose2d centerStation1Pose = FieldUtil.AprilTag.ID_13.pose2d
                     .plus(new Transform2d(Constants.ROBOT_LENGTH_WITH_BUMPERS.div(2).in(Meters), Constants.ROBOT_WIDTH_WITH_BUMPERS.div(2).unaryMinus().in(Meters), Rotation2d.kZero));
-        Pose2d station2Pose = new Pose2d(Units.inchesToMeters(67.02), Units.inchesToMeters(0), FieldUtil.AprilTag.ID_12.pose2d.getRotation())
+        Pose2d centerStation2Pose = FieldUtil.AprilTag.ID_12.pose2d
                     .plus(new Transform2d(Constants.ROBOT_LENGTH_WITH_BUMPERS.div(2).in(Meters), Constants.ROBOT_WIDTH_WITH_BUMPERS.div(2).in(Meters), Rotation2d.fromDegrees(0)));
-        System.out.println("station-1: X: " + station1Pose.getX() + ", Y: " + station1Pose.getY() + ", Angle: " + station1Pose.getRotation().getDegrees());
-        System.out.println("station-2: X: " + station2Pose.getX() + ", Y: " + station2Pose.getY() + ", Angle: " + station2Pose.getRotation().getDegrees());
+        Pose2d farStation1Pose = new Pose2d(Units.inchesToMeters(67.02), Units.inchesToMeters(317), FieldUtil.AprilTag.ID_13.pose2d.getRotation())
+                    .plus(new Transform2d(Constants.ROBOT_LENGTH_WITH_BUMPERS.div(2).in(Meters), Constants.ROBOT_WIDTH_WITH_BUMPERS.div(2).unaryMinus().in(Meters), Rotation2d.kZero));
+        Pose2d farStation2Pose = new Pose2d(Units.inchesToMeters(67.02), Units.inchesToMeters(0), FieldUtil.AprilTag.ID_12.pose2d.getRotation())
+                    .plus(new Transform2d(Constants.ROBOT_LENGTH_WITH_BUMPERS.div(2).in(Meters), Constants.ROBOT_WIDTH_WITH_BUMPERS.div(2).in(Meters), Rotation2d.fromDegrees(0)));
+        Pose2d interpolatedStation1Pose = centerStation1Pose.interpolate(farStation1Pose, 0.75);
+        Pose2d interpolatedStation2Pose = centerStation2Pose.interpolate(farStation2Pose, 0.75);
+        System.out.println("station-1: X: " + interpolatedStation1Pose.getX() + ", Y: " + interpolatedStation1Pose.getY() + ", Angle: " + interpolatedStation1Pose.getRotation().getDegrees());
+        System.out.println("station-2: X: " + interpolatedStation2Pose.getX() + ", Y: " + interpolatedStation2Pose.getY() + ", Angle: " + interpolatedStation2Pose.getRotation().getDegrees());
+        System.out.println("far station-1: X: " + farStation1Pose.getX() + ", Y: " + farStation1Pose.getY() + ", Angle: " + farStation1Pose.getRotation().getDegrees());
+        System.out.println("far station-2: X: " + farStation2Pose.getX() + ", Y: " + farStation2Pose.getY() + ", Angle: " + farStation2Pose.getRotation().getDegrees());
     }
 }
