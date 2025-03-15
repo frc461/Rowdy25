8683adcb4f59a8bdbaff938680b6cc00733c412c
ez500 <eugenezhang500@gmail.com>
2025-03-15 01:34:08 -0400

    add moer toods

--- src/main/java/frc/robot/RobotStates.java
+++ src/main/java/frc/robot/RobotStates.java
@@ -40,7 +40,7 @@ public class RobotStates {
         L1_CORAL,
         L2_CORAL,
         L3_CORAL,
-        L4_CORAL,
+        L4_CORAL, // TODO: TRY L4 PREPARE STATE
         LOW_REEF_ALGAE,
         HIGH_REEF_ALGAE,
         PROCESSOR,
