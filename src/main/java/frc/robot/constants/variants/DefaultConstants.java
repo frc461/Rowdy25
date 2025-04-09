15a9bbe10802bc669b36c869f4b2ffcfaa6afb90
ez500 <eugenezhang500@gmail.com>
2025-04-08 21:05:30 -0400

    tuning elevator

--- src/main/java/frc/robot/constants/variants/DefaultConstants.java
+++ src/main/java/frc/robot/constants/variants/DefaultConstants.java
@@ -249,8 +249,8 @@ public final class DefaultConstants {
         public static final NeutralModeValue NEUTRAL_MODE = NeutralModeValue.Coast;
 
         // mechanism characterization
-        private static final double ROTOR_TO_PULLEY_RATIO = 11.57;
-        private static final double PULLEY_CIRCUMFERENCE = 7.065;
+        private static final double ROTOR_TO_PULLEY_RATIO = 50.0 / 12.0 * 43.0 / 25.0;
+        private static final double PULLEY_CIRCUMFERENCE = 7.06858347058;
         public static final double ROTOR_TO_INCH_RATIO = ROTOR_TO_PULLEY_RATIO / PULLEY_CIRCUMFERENCE;
         private static final double STAGE_2_LOAD_LBS = 28.44;
         public static final double MASS_LBS = 23.0132625;
@@ -260,13 +260,13 @@ public final class DefaultConstants {
         public static final Translation2d ZERO_UPRIGHT_COM = new Translation2d(-11.347053, 15.125012);
 
         // pid & tolerance
-        public static final Function<Double, Double> G = (pivotDeg) -> 0.2175 * Math.sin(Math.toRadians(pivotDeg));
-        public static final double V = 0.31 / ROTOR_TO_INCH_RATIO; // 1V / (in/s) -> 1V / (rotor rps)
-        public static final double A = 0.001 / ROTOR_TO_INCH_RATIO; // 1V / (in/s^2) -> 1V / (rotor rps^2)
-        public static final double P = 0.25;
+        public static final Function<Double, Double> G = (pivotDeg) -> 0.3513 * Math.sin(Math.toRadians(pivotDeg));
+        public static final double V = 0.25 / ROTOR_TO_INCH_RATIO; // 1V / (in/s) -> 1V / (rotor rps)
+        public static final double A = 0.002 / ROTOR_TO_INCH_RATIO; // 1V / (in/s^2) -> 1V / (rotor rps^2)
+        public static final double P = 0.0;
         public static final double I = 0.0;
-        public static final double D = 0.025;
-        public static final double EXPO_V = V / 0.90; // 90% of the actual max velocity, as it will allocate 1 / 0.9 = 1.1111 times the voltage to 1 rps
+        public static final double D = 0.0;
+        public static final double EXPO_V = V / 0.05; // 90% of the actual max velocity, as it will allocate 1 / 0.9 = 1.1111 times the voltage to 1 rps
         public static final double EXPO_A = A / 0.015; // 1.5% of the actual max accel
         public static final double SAFE_TOLERANCE = 15.0;
         public static final double AT_TARGET_TOLERANCE = 2.5;
