971de11b30c19f05ad55b3c691f397e7be60f16b
ez500 <eugenezhang500@gmail.com>
2026-01-22 01:26:11 -0500

    copyrights/lefts

--- src/main/java/io/github/frc461/rowdy25/util/EquationUtil.java
+++ src/main/java/io/github/frc461/rowdy25/util/EquationUtil.java
@@ -1,5 +1,22 @@
 package io.github.frc461.rowdy25.util;
 
+/*
+ * Copyright (C) 2025-present 461 Boosters FIRST, Inc. dba Westside Robotics - The Rowdy 25.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /* Useful for smooth, logistic curves */
 public final class EquationUtil {
     /* If you need to tune your constants to apply to the exponential function, here's the link: https://www.desmos.com/calculator/yknxk8el8y */
